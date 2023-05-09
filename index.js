1d8166b765cee640af65414de1e212e2c6408ba8
Source-Code-007 <utsho926@gmail.com>
2023-05-09 22:43:52 +0600

    data add in mongodb functionality added

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.port || 6000
+const port = process.env.port || 7000
 
 app.use(cors())
 app.use(express.json())
 
 
-app.post('/users', (req,res)=>{
-    const user = req.body
-    console.log(user)
-})
+const uri = "mongodb+srv://utsho926:Wf0nvzmP0dq6OF28@cluster0.iw4kl2c.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-app.get('/', (req,res)=>{
+        const coffeesDB = client.db('coffeesDB').collection('coffees')
+
+        app.post('/addCoffee', async(req, res) => {
+            const newCoffee = req.body
+            console.log(newCoffee);
+            const result = await coffeesDB.insertOne(newCoffee);
+            res.send(result)
+        }) 
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('espresso-emporium server is running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('espresso-emporium server is running');
 })
